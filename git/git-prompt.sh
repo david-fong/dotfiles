f0b161110a914f63bfd90a1b37c23b3a3be0e3c9
david-fong <davidfong19@gmail.com>
2020-11-12 22:15:52 -0800

    say goodbye to git ps1
    It is too slow :(

--- git/git-prompt.sh
+++ git/git-prompt.sh
@@ -1,10 +1,5 @@
-
 # https://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html
 
-PS1='\[\033]0;\u@\H : $PWD\007\]\n'       # set window title
-PS1="$PS1"'\[\033[32m\]\u@\H'"'"'$SHLVL'  # green  : user
-PS1="$PS1"' \[\033[1;34m\]\@\[\033[22m\]' # blue   : time
-PS1="$PS1"' \[\033[33m\]\w'               # yellow : path
 if test -z "$WINELOADERNOEXEC"
 then
     GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -15,8 +10,17 @@ then
     then
         . "$COMPLETION_PATH/git-completion.bash"
         . "$COMPLETION_PATH/git-prompt.sh"
-        PS1="$PS1"'\[\033[36m\]`__git_ps1`' # change color to cyan
     fi
 fi
-PS1="$PS1"'\[\033[0m\]\n$ '
-
+function fast_git_ps1 ()
+{
+    printf -- "$(\git branch --show-current 2>/dev/null)"
+}
+#PROMPT_COMMAND='__git_ps1 "\[\033]0;\u@\H : $PWD\007\]\n\[\033[32m\]\u@\H \[\033[1;34m\]\@\[\033[22m\] \[\033[33m\]\w\[\033[36m\]" "\[\033[0m\]\n$ "'
+#PS1="\[\033]0;\u@\H : $PWD\007\]\n\[\033[32m\]\u@\H \[\033[1;34m\]\@\[\033[22m\] \[\033[33m\]\w\[\033[36m\] ("'`fast_git_ps1`'")\[\033[0m\]\n$ "
+function __ps1_face(){
+    declare -a FACES=('(._.)' '_(._. _)_' '(.-.)' '\(._.)/' '_( ._.) . (._. )_' '(._.) (._.)')
+    echo "${FACES[$RANDOM % ${#FACES[@]}]}"
+}
+PS1="\[\033]0;\u@\H : $PWD\007\]\n\[\033[32m\]\u@\H \[\033[1;34m\]\@\[\033[22m\] \[\033[33m\]\w\[\033[36m\] "'`__ps1_face`'"\[\033[0m\]\n$ "
+declare -rx PS1
