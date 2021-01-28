b79a7c3d8c43214479c25e4d44bb19cca6c9cb98
david-fong <davidfong19@gmail.com>
2021-01-27 21:31:20 -0800

    disable unused less keyboard commands

--- bash/xdg_compat.sh
+++ bash/xdg_compat.sh
@@ -3,8 +3,9 @@
 # https://web.archive.org/web/20180827160401/plus.google.com/+RobPikeTheHuman/posts/R58WgWwN9jp
 
 
-mkdir -p "$XDG_DATA_HOME"/bash
-mkdir -p "$XDG_DATA_HOME"/tig
+mkdir -p "${XDG_DATA_HOME}/bash"
+mkdir -p "${XDG_DATA_HOME}/tig"
+mkdir -p "${XDG_DATA_HOME}/less"
 
 declare -rx          HISTFILE="${XDG_DATA_HOME}/bash/history"
 declare -rx NODE_REPL_HISTORY="${XDG_DATA_HOME}/.node_repl_history"
@@ -15,7 +16,8 @@ declare -rx       VIMINIT='source ${XDG_CONFIG_HOME}/vim/vimrc'
 declare -rx    TIGRC_USER="${XDG_CONFIG_HOME}/git/tigrc_colorstrings"
 declare -rx PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/startup.py"
              PYTHONHOME+=":${XDG_CONFIG_HOME}/python/"
-declare -rx       LESSKEY="${XDG_CONFIG_HOME}/less/lesskey"
+declare -rx       LESSKEY="${XDG_DATA_HOME}/less/lesskey"
 
-lesskey "${XDG_CONFIG_HOME}/less/lesskey"
+# note: this must go after the declaration of LESSKEY.
+lesskey --output="${XDG_DATA_HOME}/less/lesskey" -- "${XDG_CONFIG_HOME}/less/lesskey"
 
